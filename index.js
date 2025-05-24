45433e0d721ac96354dc3fd5b4221b797ff0af35
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2025-05-24 08:32:45 +0530

    Writing and Appending text to file

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File Written');
+// Blocking Synchronous Way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File Written');
+
+// Non-blocking asynchronous Way
+fs.readFile('./txt/start.txt', 'utf-8' ,  (err,  data1) => {
+    fs.readFile('./txt/${data1}.txt', 'utf-8' ,  (err,  data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8' ,  (err,  data3) => {
+    console.log(data3);
+
+    fs.writeFile('.txt/final.txt', `${data2}\n${data2}` ,'utf-8', err => {
+        console.log('Your file has been written :)')
+    })
+});
+});
+});
+console.log('Will read file!');
