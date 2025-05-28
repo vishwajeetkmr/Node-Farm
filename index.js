1380c453fe7cbb4fdea0e313374226e26da1698d
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2025-05-28 09:20:44 +0530

    Added Route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 /////////////////////////////
 // FILES
@@ -33,8 +34,10 @@ const http = require('http');
 //////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
+    console.log(req.url);
     res.end('Hello from the server!');
 });
 
-server.listen(8000, '127.0.0.1')
-console.log('Listening to request on port 8000')
+server.listen(8001, '127.0.0.1', () => {
+    console.log('Listenting to requests on port 8001');
+}); 
