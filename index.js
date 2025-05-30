27b03bde00a4de91562760d453df475e3c55690b
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2025-05-30 09:59:11 +0530

    Changed Error Message

--- index.js
+++ index.js
@@ -40,8 +40,11 @@ const server = http.createServer((req, res) => {
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
     } else {
-        res.writeHead(404);
-        res.end('Page not found');
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
     }
 });
 
